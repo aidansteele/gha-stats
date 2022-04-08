b5a5210b10700fdf2403cd13a23a4a27785492a7
Aidan Steele <aidan.steele@glassechidna.com.au>
2022-04-08 13:32:02 +1000

    stderr

--- post.mjs
+++ post.mjs
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 
 console.log("post 1");
-execSync(`ls -lh /tmp`);
-execSync(`gha-stats stop`);
+execSync(`ls -lh /tmp 1>&2`);
+execSync(`gha-stats stop 1>&2`);
 console.log("post 2");
