9cbe837168bd871744bed52850b98d145cdf8b9c
Aidan Steele <aidan.steele@glassechidna.com.au>
2022-04-08 12:49:46 +1000

    tar order

--- start.mjs
+++ start.mjs
@@ -2,5 +2,5 @@ import { execSync } from 'child_process';
 
 const url = "https://github.com/aidansteele/gha-stats/releases/download/v0.1.0/gha-stats_0.1.0_linux_amd64.tar.gz";
 execSync(`curl -o /tmp/gha-stats.tgz -L ${url}`);
-execSync(`tar -xvf -C /usr/bin /tmp/gha-stats.tgz`);
+execSync(`tar -C /usr/bin -xvf /tmp/gha-stats.tgz`);
 execSync(`gha-stats start`);
